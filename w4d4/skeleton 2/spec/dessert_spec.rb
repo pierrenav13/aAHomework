9762a0af0cbaac51dfa1937c0c87aca9a4febaf2
pierre <pnavarin@gmail.com>
2021-09-02 08:54:40 -0400

    Modified dessert_spec

--- w4d4/skeleton 2/spec/dessert_spec.rb
+++ w4d4/skeleton 2/spec/dessert_spec.rb
@@ -7,27 +7,39 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  let(:dessert) {Dessert.new('Cookie', 4, chef)}
 
   describe "#initialize" do
-    it "sets a type"
-
-    it "sets a quantity"
-
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "sets a type" do
+      expect(dessert.type).to eq(type)
+    end
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(quantity)
+    end
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients.empty?).to be true
+    end
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new('', 'i', chef)}to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      expect(dessert.add_ingredient('Flour'))to eq(['Flour'])
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      expect
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      expect 
+    end
 
     it "raises an error if the amount is greater than the quantity"
   end
