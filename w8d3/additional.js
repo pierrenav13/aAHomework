1e006977e5c554e06ba463ebfa2b3900c4ce35e0
pierre <pnavarin@gmail.com>
2021-09-29 10:49:08 -0400

    Worked on phase 2

--- w8d3/additional.js
+++ w8d3/additional.js
@@ -1,11 +1,29 @@
 function titleize(names){
     names.forEach(name => {
-        console.log(`Mx. ${name} Jingleheimer Schmidt`)
-    })
-}
+        console.log(`Mx. ${name} Jingleheimer Schmidt`);
+    });
+};
 
 titleize(["Mary", "Brian", "Leo"])
 
 function Elephant(name, height, tricks){
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+};
 
-}
+Elephant.prototype.trumpet = function(){
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!`);
+};
+
+Elephant.prototype.grow = function(){
+    this.height += 12;
+};
+
+Elephant.prototype.addTrick= function(trick){
+    this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function(){
+
+};
