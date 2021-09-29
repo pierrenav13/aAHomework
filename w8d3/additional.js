d3b9e1cfc2ca84827f1016e95b8d31bb856ed95c
pierre <pnavarin@gmail.com>
2021-09-29 08:51:36 -0400

    Finished Phase 1

---
+++ w8d3/additional.js
@@ -0,0 +1,11 @@
+function titleize(names){
+    names.forEach(name => {
+        console.log(`Mx. ${name} Jingleheimer Schmidt`)
+    })
+}
+
+titleize(["Mary", "Brian", "Leo"])
+
+function Elephant(name, height, tricks){
+
+}
